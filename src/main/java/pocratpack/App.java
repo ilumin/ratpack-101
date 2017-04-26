b73a6bb0abf69f2a4d0684ca50509844faa1ef8c
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-26 17:26:31 +0700

    UPDATE app - get data from DB using DSL

--- src/main/java/pocratpack/App.java
+++ src/main/java/pocratpack/App.java
@@ -1,8 +1,17 @@
 package pocratpack;
 
+import jooq.tables.Todo;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import ratpack.guice.Guice;
 import ratpack.hikari.HikariModule;
+import ratpack.jackson.Jackson;
 import ratpack.server.RatpackServer;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 public class App {
 
@@ -18,7 +27,12 @@ public class App {
             )
             .handlers(chain -> chain
                 .all(new CORSHandler())
-                .get(ctx -> ctx.render("Dee Jaa"))
+                .get(ctx -> {
+                    DataSource ds = ctx.get(DataSource.class);
+                    DSLContext create = DSL.using(ds, SQLDialect.H2);
+                    List<Map<String, Object>> maps = create.select().from(Todo.TODO).fetch().intoMaps();
+                    ctx.render(Jackson.json(maps));
+                })
                 .get("foo", ctx -> ctx.render("foo bar"))
             ));
     }
--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -4,4 +4,11 @@ CREATE TABLE todo (
   `title` varchar(256),
   `completed` bool default false,
   `order` int default null
-)
+);
+
+INSERT INTO todo (`title`, `completed`, `order`)
+VALUES
+  ('Yikes Subject 01', 1, 1),
+  ('Yikes Subject 02', 0, 2),
+  ('Yikes Subject 03', 0, 3),
+;
