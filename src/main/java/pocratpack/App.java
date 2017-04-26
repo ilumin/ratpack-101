8f65d92879375a045e4167360380eb34776b46de
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-26 15:30:14 +0700

    UPDATE app move header into handler

--- src/main/java/pocratpack/App.java
+++ src/main/java/pocratpack/App.java
@@ -1,6 +1,5 @@
 package pocratpack;
 
-import ratpack.http.MutableHeaders;
 import ratpack.server.RatpackServer;
 
 public class App {
@@ -8,12 +7,7 @@ public class App {
     public static void main(String... args) throws Exception {
         RatpackServer.start(serverSpec -> serverSpec
                 .handlers(chain -> chain
-                        .all(ctx -> {
-                            MutableHeaders headers = ctx.getResponse().getHeaders();
-                            headers.set("Access-Control-Allow-Origin", "*");
-                            headers.set("Access-Control-Allow-Headers", "x-request-with, origin, content-type, accept");
-                            ctx.next();
-                        })
+                        .all(new CORSHandler())
                         .get(ctx -> ctx.render("Dee Jaa"))
                         .get("foo", ctx -> ctx.render("foo bar"))
                 ));
