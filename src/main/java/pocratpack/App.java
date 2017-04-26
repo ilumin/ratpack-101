6fe5f340dfefe0f8fa79bef9790cbb46411a8e4e
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-26 17:15:17 +0700

    UPDATE app - inject Hikari module using Guice * Guice is DI package from google

--- src/main/java/pocratpack/App.java
+++ src/main/java/pocratpack/App.java
@@ -1,16 +1,26 @@
 package pocratpack;
 
+import ratpack.guice.Guice;
+import ratpack.hikari.HikariModule;
 import ratpack.server.RatpackServer;
 
 public class App {
 
     public static void main(String... args) throws Exception {
         RatpackServer.start(serverSpec -> serverSpec
-                .handlers(chain -> chain
-                        .all(new CORSHandler())
-                        .get(ctx -> ctx.render("Dee Jaa"))
-                        .get("foo", ctx -> ctx.render("foo bar"))
-                ));
+            .registry(Guice.registry(bindingsSpec -> bindingsSpec
+                .module(HikariModule.class, hikariConfig -> {
+                    hikariConfig.setDataSourceClassName("org.h2.jdbcx.JdbcDataSource");
+                    hikariConfig.addDataSourceProperty(
+                        "URL",
+                        "jdbc:h2:mem:todo;INIT=RUNSCRIPT FROM 'classpath:/init.sql'");
+                }))
+            )
+            .handlers(chain -> chain
+                .all(new CORSHandler())
+                .get(ctx -> ctx.render("Dee Jaa"))
+                .get("foo", ctx -> ctx.render("foo bar"))
+            ));
     }
 
 }
