3581b8311e63470206b5bd90daa6fbd2a20d46bc
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-27 12:00:34 +0700

    UPDATE app - apply byMethodSpec

--- src/main/java/pocratpack/App.java
+++ src/main/java/pocratpack/App.java
@@ -3,6 +3,7 @@ package pocratpack;
 import ratpack.exec.Promise;
 import ratpack.guice.Guice;
 import ratpack.hikari.HikariModule;
+import ratpack.http.Response;
 import ratpack.jackson.Jackson;
 import ratpack.server.RatpackServer;
 
@@ -18,21 +19,31 @@ public class App {
                         "jdbc:h2:mem:todo;INIT=RUNSCRIPT FROM 'classpath:/init.sql'");
                 })
                 .module(TodoModule.class)
+                .bindInstance(new CORSHandler())
             ))
             .handlers(chain -> chain
-                .all(new CORSHandler())
-                .get(ctx -> {
+                .all(CORSHandler.class)
+                .path(ctx -> {
                     TodoRepository repository = ctx.get(TodoRepository.class);
-                    repository.getAll()
-                        .map(Jackson::json)
-                        .then(ctx::render);
-                })
-                .post(ctx -> {
-                    TodoRepository repository = ctx.get(TodoRepository.class);
-                    Promise<TodoModel> todo = ctx.parse(Jackson.fromJson(TodoModel.class));
-                    todo.flatMap(repository::add)
-                        .map(Jackson::json)
-                        .then(ctx::render);
+                    Response response = ctx.getResponse();
+                    ctx.byMethod(byMethodSpec -> byMethodSpec
+                        .options(() -> {
+                            response.getHeaders().set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE");
+                            response.send();
+                        })
+                        .get(() -> {
+                            repository.getAll()
+                                .map(Jackson::json)
+                                .then(ctx::render);
+                        })
+                        .post(() -> {
+                            Promise<TodoModel> todo = ctx.parse(Jackson.fromJson(TodoModel.class));
+                            todo.flatMap(repository::add)
+                                .map(Jackson::json)
+                                .then(ctx::render);
+                        })
+                        .delete(() -> repository.deleteAll().then(response::send))
+                    );
                 })
             ));
     }
