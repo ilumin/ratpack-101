76f21ac5a85c9f51406d8bed6e35382992d51568
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-04-27 11:51:03 +0700

    UPDATE todo repository - add CRUD

--- src/main/java/pocratpack/App.java
+++ src/main/java/pocratpack/App.java
@@ -1,12 +1,9 @@
 package pocratpack;
 
-import ratpack.exec.Promise;
 import ratpack.guice.Guice;
 import ratpack.hikari.HikariModule;
 import ratpack.jackson.Jackson;
 import ratpack.server.RatpackServer;
-
-import java.util.List;
 
 public class App {
 
@@ -24,11 +21,6 @@ public class App {
             .handlers(chain -> chain
                 .all(new CORSHandler())
                 .get(ctx -> {
-                    TodoRepository repository = ctx.get(TodoRepository.class);
-                    Promise<List<TodoModel>> todos = repository.getAll();
-                    todos.then(t -> ctx.render(Jackson.json(t)));
-                })
-                .get("lambda", ctx -> {
                     TodoRepository repository = ctx.get(TodoRepository.class);
                     repository.getAll()
                         .map(Jackson::json)
--- src/main/java/pocratpack/TodoRepository.java
+++ src/main/java/pocratpack/TodoRepository.java
@@ -1,15 +1,16 @@
 package pocratpack;
 
 import jooq.tables.Todo;
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.SelectJoinStep;
+import jooq.tables.records.TodoRecord;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 import ratpack.exec.Blocking;
+import ratpack.exec.Operation;
 import ratpack.exec.Promise;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Map;
 
 public class TodoRepository {
     private final DSLContext create;
@@ -21,5 +22,34 @@ public class TodoRepository {
     public Promise<List<TodoModel>> getAll() {
         SelectJoinStep all = create.select().from(Todo.TODO);
         return Blocking.get(() -> all.fetchInto(TodoModel.class));
+    }
+
+    public Promise<TodoModel> getById(Long id) {
+        SelectConditionStep where = create.select().from(Todo.TODO).where(Todo.TODO.ID.eq(id));
+        return Blocking.get(() -> where.fetchOne().into(TodoModel.class));
+    }
+
+    public Promise<TodoModel> add(TodoModel todo) {
+        TodoRecord record = create.newRecord(Todo.TODO, todo);
+        return Blocking.op(record::store)
+                .next(Blocking.op(record::refresh))
+                .map(() -> record.into(TodoModel.class));
+    }
+
+    public Promise<TodoModel> update(Map<String, Object> todo) {
+        TodoRecord record = create.newRecord(Todo.TODO, todo);
+        return Blocking.op(() -> create.executeUpdate(record))
+                .next(Blocking.op(record::refresh))
+                .map(() -> record.into(TodoModel.class));
+    }
+
+    public Operation delete(Long id) {
+        DeleteConditionStep<TodoRecord> deleteWhereId = create.deleteFrom(Todo.TODO).where(Todo.TODO.ID.eq(id));
+        return Blocking.op(deleteWhereId::execute);
+    }
+
+    public Operation deleteAll() {
+        DeleteWhereStep<TodoRecord> delete = create.deleteFrom(Todo.TODO);
+        return Blocking.op(delete::execute);
     }
 }
